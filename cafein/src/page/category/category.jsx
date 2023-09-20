e885cf247908fe10e386d5cbe2a7e8907777efbe
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2023-09-20 18:51:13 +0900

    category page modified

--- cafein/src/page/category/category.jsx
+++ cafein/src/page/category/category.jsx
@@ -1,55 +1,93 @@
-import React, { useState } from "react";
-import '../../style/categorypage/category.scss';
+import React, { useState, useEffect } from "react";
+import '../../style/categorypage/category.scss'; // 스타일 파일 경로를 수정하세요.
+import tagInfo from './coffeedata.js'; // JSON 파일 경로를 수정하세요.
 
-// 각 태그에 대한 정보를 담은 객체 배열
-const tagInfo = {
-  '스타벅스': '스타벅스에 대한 정보',
-  '이디야': '이디야에 대한 정보',
-  '투섬': '투섬에 대한 정보',
-  '빽다방': '빽다방에 대한 정보',
-  '메가커피': '메가커피에 대한 정보',
-};
+
+const TagList = ({ tags, onTagClick, selectedTagId }) => {
+  return (
+    <div className="category">
+      {tags.map((tag) => (
+        <button
+          key={tag.id}
+          className={`category-tag ${selectedTagId === tag.id ? 'selected' : ''}`}
+          onClick={() => onTagClick(tag.id)}
+        >
+          {tag.name}
+        </button>
+      ))}
+    </div>
+  )
+}
+
+
 
 function Category() {
-  const [selectedTags, setSelectedTags] = useState([]);
-  const [tagInfoToShow, setTagInfoToShow] = useState(null);
+  const [selectedTagId, setSelectedTag] = useState(null);
+  const [searchQuery, setSearchQuery] = useState("");
 
-  const tags = ['스타벅스', '이디야', '투섬', '빽다방', '메가커피'];
-
-  const handleTagClick = (tagId) => {
-    if (selectedTags.includes(tagId)) {
-      setSelectedTags(selectedTags.filter((t) => t !== tagId));
-    } else {
-      setSelectedTags([...selectedTags, tagId]);
+  const handleTagClick = (tag) => {
+    if (selectedTagId === tag) {
+      setSelectedTag(null);
+    }
+    else {
+      setSelectedTag(tag);
     }
 
-    // 선택한 태그에 대한 정보를 설정
-    setTagInfoToShow(tagInfo[tagId]);
+
   };
+
+  const CafeName = tagInfo.find((tag) => tag.id === selectedTagId)?.name
+  const CafeContent = tagInfo.find((tag) => tag.id === selectedTagId)?.content
+
+  //검색어 결과 업데이트
+  const filtered = tagInfo.filter((tag) =>
+    tag.content.toLowerCase().includes(searchQuery.toLowerCase())
+  );
 
   return (
     <div>
+
       <div className="category-title">메뉴</div>
-      <div className="category">
-        {tags.map((tag) => (
-          <div
-            key={tag}
-            className={`category-tag ${selectedTags.includes(tag) ? 'selected' : ''}`}
-            onClick={() => handleTagClick(tag)}
-          >
-            {tag}
-          </div>
-        ))}
-      </div>
-      {/* 선택한 태그에 대한 정보를 출력 */}
-      {tagInfoToShow && (
-        <div className="tag-info">
-          <h3>{selectedTags[selectedTags.length - 1]} 정보</h3>
-          <p>{tagInfoToShow}</p>
+      <div >
+        
+        <input
+            className="category-search"
+            type="text"
+            placeholder="음료 검색"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+          
+        
+        <div>
+          <TagList
+            tags={tagInfo}
+            onTagClick={handleTagClick}
+            selectedTagId={selectedTagId}
+          />
         </div>
-      )}
+        {/* 선택한 태그에 대한 정보를 출력 */}
+        {selectedTagId && (
+          <div className="tag-info">
+            <h3>{CafeName} 정보</h3>
+            <p>{CafeContent}</p>
+          </div>
+        )}
+        {searchQuery && (
+          <div className="search-results">
+            
+            <ul>
+              {filtered.map((tag) => (
+                <li key={tag.id}>{tag.name}{tag.content}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+
     </div>
   );
 }
 
 export default Category;
+
---
+++ cafein/src/page/category/coffeedata.js
@@ -0,0 +1,11 @@
+const coffeeData= [
+    { id: 1, name: "스타벅스", content: "아메리카노" },
+    { id: 2, name: "이디야", content: "아메리카노"},
+    { id: 3, name: "투섬",  content: "카라멜마키야또"},
+    { id: 4, name: "빽다방", content: "카라멜마키야또" },
+    { id: 5, name: "메가커피",  content: "카페라떼"},
+    
+]
+
+
+export default coffeeData
