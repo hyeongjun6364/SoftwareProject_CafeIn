f85364ad61818154024fe1e338be0e9241b12787
kscold <chan6502@gmail.com>
2023-10-06 22:45:54 +0900

    이디아 백엔드 업데이트

--- cafein_backend/src/api/cafe/cafe.ctrl.js
+++ cafein_backend/src/api/cafe/cafe.ctrl.js
@@ -1,26 +1,38 @@
-// src/api/cafe/cafe.ctrl.js// src/api/cafe/cafe.ctrl.js
+// src/api/cafe/cafe.ctrl.js
 import fs from 'fs/promises';
 import path from 'path';
 import Cafe from '../../models/cafe';
 
 export const getEdiyaMenu = async (ctx) => {
   try {
+    // MongoDB에서 저장된 데이터를 불러옵니다.
+    const existingCafes = await Cafe.find({}, 'name').lean();
+
     // 절대 경로로 JSON 파일을 읽어옵니다.
     const filePath = path.join(__dirname, 'cafeinfo', 'ediya_menu.json');
     const data = await fs.readFile(filePath, 'utf8');
     const jsonData = JSON.parse(data);
 
-    // MongoDB에 저장할 데이터 형식을 Cafe 모델과 일치시킵니다.
-    const cafesToSave = jsonData.map((item, index) => ({
-      id: index + 1, // 일련번호를 1부터 부여
-      name: item.prodName, // prodName을 name 필드에 저장
-      image: item.prodImage, // prodImage를 image 필드에 저장
-      tag: item.tag || [], // tag 필드를 선택적으로 저장 (없을 경우 빈 배열)
-      menu: item.menu || [], // menu 필드를 선택적으로 저장 (없을 경우 빈 배열)
-    }));
+    // JSON 데이터에서 이미 MongoDB에 있는 name 필드의 값을 제외하고 새로운 데이터로 만듭니다.
+    const newCafes = jsonData
+      .filter(
+        (item) =>
+          !existingCafes.some((existing) => existing.name === item.prodName),
+      )
+      .map((item, index) => ({
+        id: index + 1, // 일련번호를 1부터 부여,
+        cafeid: index + 1, // 일련번호를 1부터 부여,
+        name: item.prodName, // prodName을 name 필드에 저장
+        cafe: item.prodCafe,
+        content: item.prodContent || '',
+        image: item.prodImage, // prodImage를 image 필드에 저장
+        tag: item.prodTag || [], // tag 필드를 선택적으로 저장 (없을 경우 빈 배열)
+      }));
 
-    // MongoDB에 저장합니다.
-    await Cafe.insertMany(cafesToSave);
+    // MongoDB에 새로운 데이터를 추가합니다.
+    if (newCafes.length > 0) {
+      await Cafe.insertMany(newCafes);
+    }
 
     ctx.body = '데이터가 성공적으로 저장되었습니다.';
   } catch (err) {
