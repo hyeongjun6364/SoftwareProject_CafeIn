42b4a6ce6da1da97b9497ff50cae2fa998cd5231
kscold <chan6502@gmail.com>
2023-10-12 11:39:33 +0900

    백엔드 카페 api 업데이트

--- cafein/src/page/category/category.jsx
+++ cafein/src/page/category/category.jsx
@@ -1,9 +1,194 @@
+// import React, { useState, useEffect } from "react"
+// import "../../style/categorypage/category.scss" // 스타일 파일 경로를 수정하세요.
+// import { coffee, cafename } from "./coffeedata.js" // JSON 파일 경로를 수정하세요.
+// // import coffeeData from "./Data.json"
+// import { useNavigate } from "react-router-dom"
+// import axios from "axios"
+
+// const TagList = ({ tags, onTagClick, selectedTagId }) => {
+//   return (
+//     <div className="category">
+//       {tags.map((tag) => (
+//         <button
+//           key={tag.id}
+//           className={`category-tag ${
+//             selectedTagId === tag.id ? "selected" : ""
+//           }`}
+//           onClick={() => onTagClick(tag.id)}
+//         >
+//           {tag.name}
+//         </button>
+//       ))}
+//     </div>
+//   )
+// }
+
+// function Category() {
+//   const [selectedTagCafeId, setSelectedCafeTag] = useState(null)
+//   const [selectedTagCoffeeId, setSelectedTagCoffee] = useState(null)
+//   const [searchQuery, setSearchQuery] = useState("")
+//   const [posts, setPosts] = useState([]) // 데이터를 저장할 상태 추가
+//   const [currentPage, setCurrentPage] = useState(1)
+//   const navigate = useNavigate()
+
+//   useEffect(() => {
+//     setSelectedCafeTag(null)
+//     setSelectedTagCoffee(null)
+//   }, [searchQuery])
+
+//   useEffect(() => {
+//     async function fetchData() {
+//       try {
+//         const response = await axios.get(
+//           `http://localhost:4000/api/cafe/db_get_starbucks_menu?page=${currentPage}`
+//         )
+//         setPosts(response.data) // 데이터를 상태로 업데이트
+//       } catch (error) {
+//         console.log(error)
+//       }
+//     }
+//     fetchData()
+//   }, [currentPage])
+
+//   const handleTagClick = (tag) => {
+//     if (selectedTagCafeId === tag) {
+//       setSelectedCafeTag(null)
+//       setSelectedTagCoffee(null)
+//       setSearchQuery("")
+//     } else {
+//       setSelectedCafeTag(tag)
+//       setSelectedTagCoffee(null)
+//       setSearchQuery("")
+//       setCurrentPage(1)
+//     }
+//   }
+
+//   const handleCoffeeTagClick = (tag) => {
+//     if (selectedTagCoffeeId === tag) {
+//       setSelectedTagCoffee(null)
+//       setSelectedCafeTag(null)
+//       setSearchQuery("")
+//     } else {
+//       setSelectedTagCoffee(tag)
+//       setSelectedCafeTag(null)
+//       setSearchQuery("")
+//     }
+//   }
+
+//   const handleCoffeeDetail = (coffeeId, cafename) => {
+//     navigate(`/category/${cafename}/${coffeeId}`)
+//   }
+
+//   const CafeName = posts.find((tag) => tag.id === selectedTagCafeId)?.cafe
+//   const CafeContent = coffee.find(
+//     (tag) => tag.id === selectedTagCoffeeId
+//   )?.content
+
+//   const filteredCafe = posts.filter((tag) =>
+//     tag.name.toLowerCase().includes(searchQuery.toLowerCase())
+//   )
+
+//   return (
+//     <div style={{ margin: "0 5%" }}>
+//       <div className="category-title">메뉴</div>
+//       <div>
+//         <input
+//           className="category-search"
+//           type="text"
+//           placeholder="음료 검색"
+//           value={searchQuery}
+//           onChange={(e) => setSearchQuery(e.target.value)}
+//         />
+
+//         <div>
+//           <div className="category-cafe">카페이름</div>
+
+//           <TagList
+//             tags={cafename}
+//             onTagClick={handleTagClick}
+//             selectedTagId={selectedTagCafeId}
+//           />
+//         </div>
+//         <br />
+//         <div>
+//           <div className="category-cafe">커피이름</div>
+
+//           <TagList
+//             tags={coffee}
+//             onTagClick={handleCoffeeTagClick}
+//             selectedTagId={selectedTagCoffeeId}
+//           />
+//         </div>
+//         {/* 선택한 태그에 대한 정보를 출력 */}
+//         <br />
+//         <div className="category-cafe">총 메뉴</div>
+//         {selectedTagCafeId && (
+//           <div className="tag-info">
+//             <div className="coffee-grid">
+//               {fetchData.map(
+//                 (tag) =>
+//                   tag.cafeid === selectedTagCafeId && (
+//                     <React.Fragment key={tag.id}>
+//                       <div className="coffee-item">
+//                         <img
+//                           src={tag.image}
+//                           alt={tag.name}
+//                           className="category-image"
+//                           onClick={() => handleCoffeeDetail(tag.id, tag.cafe)}
+//                         />
+//                         <p>{tag.name}</p>
+//                       </div>
+//                     </React.Fragment>
+//                   )
+//               )}
+//             </div>
+//           </div>
+//         )}
+
+//         {selectedTagCoffeeId && (
+//           <div className="tag-info">
+//             <h3>{CafeName} 정보</h3>
+//             <p>{CafeContent}</p>
+//           </div>
+//         )}
+//         {searchQuery && (
+//           <div className="search-results">
+//             <div className="coffee-grid">
+//               {filteredCafe.map(
+//                 (tag) =>
+//                   searchQuery === tag.name && (
+//                     <React.Fragment key={tag.id}>
+//                       <div className="coffee-item">
+//                         <img
+//                           src={tag.image}
+//                           alt={tag.name}
+//                           className="category-image"
+//                           onClick={() => handleCoffeeDetail(tag.id, tag.cafe)}
+//                         />
+//                         <p>{tag.name}</p>
+//                       </div>
+//                     </React.Fragment>
+//                   )
+//               )}
+//             </div>
+//           </div>
+//         )}
+
+//         <div></div>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default Category
+
 import React, { useState, useEffect } from "react"
-import "../../style/categorypage/category.scss" // 스타일 파일 경로를 수정하세요.
-import { coffee, cafename } from "./coffeedata.js" // JSON 파일 경로를 수정하세요.
-import coffeeData from "./Data.json"
-import { useNavigate } from "react-router-dom"
 import axios from "axios"
+import { useNavigate } from "react-router-dom"
+
+import "../../style/categorypage/category.scss"
+import { coffee, cafename } from "./coffeedata.js"
+
 const TagList = ({ tags, onTagClick, selectedTagId }) => {
   return (
     <div className="category">
@@ -22,35 +207,65 @@ const TagList = ({ tags, onTagClick, selectedTagId }) => {
   )
 }
 
+const CoffeeList = ({ coffees }) => {
+  const [selectedCafe, setSelectedCafe] = useState("starbucks")
+
+  const filteredCoffees = coffees.filter(
+    (coffee) => coffee.cafe === selectedCafe
+  )
+
+  return (
+    <div>
+      <div>
+        <select
+          value={selectedCafe}
+          onChange={(e) => setSelectedCafe(e.target.value)}
+        >
+          <option value="starbucks">Starbucks</option>
+          <option value="Mega">Mega</option>
+          <option value="Bbaek">Bbaek</option>
+          {/* Add options for other cafes */}
+        </select>
+      </div>
+      <div>
+        {filteredCoffees.map((coffee) => (
+          <div key={coffee.id}>
+            <h2>{coffee.name}</h2>
+            <p>{coffee.content}</p>
+            <img src={coffee.image} alt={coffee.name} />
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
 function Category() {
   const [selectedTagCafeId, setSelectedCafeTag] = useState(null)
   const [selectedTagCoffeeId, setSelectedTagCoffee] = useState(null)
   const [searchQuery, setSearchQuery] = useState("")
   const [posts, setPosts] = useState([])
+  const [currentPage, setCurrentPage] = useState(1)
   const navigate = useNavigate()
+
   useEffect(() => {
     setSelectedCafeTag(null)
     setSelectedTagCoffee(null)
   }, [searchQuery])
 
-  // API완성되면 쓰일 임시 코드
-  // 비동기통신을 하기위해 async, await를 useEffect 함수내에서 직접 썻지만
-  //  경고 메시지가 나와서 함수를 정의하고 이 함수를 반환하는 식으로 바꿈
-
-  // useEffect(() => {
-  //   async function fetchData(){
-  //       try {
-  //           await axios.get('https://jsonplaceholder.typicode.com/posts')
-  //           .then((response) => {
-  //             setPosts(response.data);
-  //           })
-  //         } catch (error) {
-  //           console.log(error)
-  //         }
-  //   }
-  //   fetchData()
-
-  // },[])
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get(
+          `http://localhost:4000/api/cafe/db_get_starbucks_menu?page=${currentPage}`
+        )
+        setPosts(response.data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchData()
+  }, [currentPage])
 
   const handleTagClick = (tag) => {
     if (selectedTagCafeId === tag) {
@@ -61,8 +276,10 @@ function Category() {
       setSelectedCafeTag(tag)
       setSelectedTagCoffee(null)
       setSearchQuery("")
+      setCurrentPage(1)
     }
   }
+
   const handleCoffeeTagClick = (tag) => {
     if (selectedTagCoffeeId === tag) {
       setSelectedTagCoffee(null)
@@ -79,13 +296,12 @@ function Category() {
     navigate(`/category/${cafename}/${coffeeId}`)
   }
 
-  const CafeName = coffeeData.find((tag) => tag.id === selectedTagCafeId)?.cafe
+  const CafeName = posts.find((tag) => tag.id === selectedTagCafeId)?.cafe
   const CafeContent = coffee.find(
     (tag) => tag.id === selectedTagCoffeeId
   )?.content
 
-  //검색어 결과 업데이트
-  const filteredCafe = coffeeData.filter((tag) =>
+  const filteredCafe = posts.filter((tag) =>
     tag.name.toLowerCase().includes(searchQuery.toLowerCase())
   )
 
@@ -120,13 +336,12 @@ function Category() {
             selectedTagId={selectedTagCoffeeId}
           />
         </div>
-        {/* 선택한 태그에 대한 정보를 출력 */}
         <br />
         <div className="category-cafe">총 메뉴</div>
         {selectedTagCafeId && (
           <div className="tag-info">
             <div className="coffee-grid">
-              {coffeeData.map(
+              {posts.map(
                 (tag) =>
                   tag.cafeid === selectedTagCafeId && (
                     <React.Fragment key={tag.id}>
