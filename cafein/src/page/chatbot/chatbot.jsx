eece4509cb8c01d7f41232e0676a8dd4c91c9a36
kscold <chan6502@gmail.com>
2023-11-20 02:16:50 +0900

    챗봇 앵커 태그 업데이트

--- cafein/src/page/chatbot/chatbot.jsx
+++ cafein/src/page/chatbot/chatbot.jsx
@@ -1,235 +1,3 @@
-// import React, { useState, useEffect } from "react"
-// import axios from "axios"
-// import "../../style/chatbot/chatbot.scss"
-
-// function Chatbot() {
-//   const [questions, setQuestions] = useState([])
-//   const [input, setInput] = useState("")
-
-//   useEffect(() => {
-//     const chatquestions = document.querySelector(".chat-questions")
-//     chatquestions.scrollTop = chatquestions.scrollHeight
-//   }, [questions])
-
-//   const handleSend = () => {
-//     const userQuestion = input
-//     setQuestions([...questions, { text: userQuestion, sender: "user" }])
-//     setInput("")
-
-//     // Add the loading class to show the loading animation
-//     const inputElement = document.querySelector(".chat-input-field")
-//     inputElement.classList.add("loading")
-
-//     // Send the user's question to the server using Axios
-//     axios
-//       .post("http://localhost:4000/api/gpt/ask", { question: userQuestion })
-//       .then((response) => {
-//         // Check if 'answer' property exists in the response.data
-//         if (response.data && response.data.answer) {
-//           // Extract URL from the answer
-//           const dynamicUrlRegex =
-//             /<a href="([^"]+)" target="_blank">([^<]+)<\/a>/
-//           const match = response.data.answer.match(dynamicUrlRegex)
-
-//           if (match) {
-//             // If URL exists, show both the answer and the URL
-//             setQuestions((prevquestions) => [
-//               ...prevquestions,
-//               { text: response.data.answer, sender: "bot" },
-//               {
-//                 text: (
-//                   <p>
-//                     <a
-//                       href={match[1]}
-//                       target="_blank"
-//                       rel="noopener noreferrer"
-//                     >
-//                       {match[2]}
-//                     </a>
-//                   </p>
-//                 ),
-//                 sender: "bot",
-//               },
-//             ])
-//           } else {
-//             // If no URL, show only the answer
-//             setQuestions((prevquestions) => [
-//               ...prevquestions,
-//               { text: response.data.answer, sender: "bot" },
-//             ])
-//           }
-//         } else {
-//           console.error("Invalid response format:", response.data)
-//         }
-//       })
-//       .catch((error) => {
-//         console.error("Error sending question to the server:", error)
-//       })
-//       .finally(() => {
-//         // Remove the loading class to stop the loading animation
-//         inputElement.classList.remove("loading")
-//       })
-//   }
-
-//   return (
-//     <div className="chatbot">
-//       <h2>Chatbot</h2>
-//       <div className="chat-questions chat-messages">
-//         {questions.map((question, id) => (
-//           <div
-//             key={id}
-//             className={`chat-message ${
-//               question.sender === "user" ? "user" : "bot"
-//             }`}
-//           >
-//             <p>{question.text}</p>
-//           </div>
-//         ))}
-//       </div>
-
-//       <div className="chat-input">
-//         <input
-//           className="chat-input-field"
-//           placeholder="글 작성"
-//           value={input}
-//           onChange={(e) => setInput(e.target.value)}
-//           onKeyPress={(e) =>
-//             e.key === "Enter" && !e.shiftKey ? handleSend() : null
-//           }
-//         />
-
-//         <button className="chat-input-button" onClick={handleSend}>
-//           전송
-//         </button>
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default Chatbot
-
-// import React, { useState, useEffect } from "react"
-// import axios from "axios"
-// import "../../style/chatbot/chatbot.scss"
-
-// function Chatbot() {
-//   const [questions, setQuestions] = useState([])
-//   const [input, setInput] = useState("")
-//   const [isLoading, setIsLoading] = useState(false)
-
-//   useEffect(() => {
-//     const chatquestions = document.querySelector(".chat-questions")
-//     chatquestions.scrollTop = chatquestions.scrollHeight
-//   }, [questions])
-
-//   const handleInputChange = (e) => {
-//     setInput(e.target.value)
-
-//     // 텍스트가 변경될 때마다 클래스 토글
-//     const inputField = e.target
-//     inputField.classList.add("text-changed")
-
-//     console.log("Text changed. Adding class...")
-
-//     setTimeout(() => {
-//       inputField.classList.remove("text-changed")
-//       console.log("Class removed after 300ms.")
-//     }, 300) // transition 지속 시간과 동일한 시간으로 설정
-//   }
-
-//   const handleSend = () => {
-//     const userQuestion = input
-//     setQuestions([...questions, { text: userQuestion, sender: "user" }])
-//     setInput("")
-//     setIsLoading(true)
-
-//     // Send the user's question to the server using Axios
-//     axios
-//       .post("http://localhost:4000/api/gpt/ask", { question: userQuestion })
-//       .then((response) => {
-//         // Check if 'answer' property exists in the response.data
-//         if (response.data && response.data.answer) {
-//           // Extract URL from the answer
-//           const dynamicUrlRegex =
-//             /<a href="([^"]+)" target="_blank">([^<]+)<\/a>/
-//           const match = response.data.answer.match(dynamicUrlRegex)
-
-//           if (match) {
-//             // If URL exists, show both the answer and the URL
-//             setQuestions((prevquestions) => [
-//               ...prevquestions,
-//               { text: response.data.answer, sender: "bot" },
-//               {
-//                 text: (
-//                   <p>
-//                     <a
-//                       href={match[1]}
-//                       target="_blank"
-//                       rel="noopener noreferrer"
-//                     >
-//                       {match[2]}
-//                     </a>
-//                   </p>
-//                 ),
-//                 sender: "bot",
-//               },
-//             ])
-//           } else {
-//             // If no URL, show only the answer
-//             setQuestions((prevquestions) => [
-//               ...prevquestions,
-//               { text: response.data.answer, sender: "bot" },
-//             ])
-//           }
-//         } else {
-//           console.error("Invalid response format:", response.data)
-//         }
-//       })
-//       .catch((error) => {
-//         console.error("Error sending question to the server:", error)
-//       })
-//       .finally(() => {
-//         setIsLoading(false)
-//       })
-//   }
-
-//   return (
-//     <div className="chatbot">
-//       <h2>Chatbot</h2>
-//       <div className="chat-questions chat-messages">
-//         {questions.map((question, id) => (
-//           <div
-//             key={id}
-//             className={`chat-message ${
-//               question.sender === "user" ? "user" : "bot"
-//             }`}
-//           >
-//             <p>{question.text}</p>
-//           </div>
-//         ))}
-//       </div>
-
-//       <div id="wave" className="chat-input">
-//         <input
-//           className={`chat-input-field ${isLoading ? "loading" : ""}`}
-//           placeholder="글 작성"
-//           value={input}
-//           onChange={handleInputChange}
-//           onKeyPress={(e) =>
-//             e.key === "Enter" && !e.shiftKey ? handleSend() : null
-//           }
-//         />
-
-//         <button className="chat-input-button" onClick={handleSend}>
-//           전송
-//         </button>
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default Chatbot
-
 import React, { useState, useEffect, useRef } from "react"
 import axios from "axios"
 import "../../style/chatbot/chatbot.scss"
@@ -286,28 +54,73 @@ function Chatbot() {
     }
   }
 
+  // 디버깅 코드
+  // const handleResponse = (data) => {
+  //   if (data && data.answer) {
+  //     const dynamicUrlRegex = /<a href="([^"]+)" target="_blank">([^<]+)<\/a>/
+  //     const match = data.answer.match(dynamicUrlRegex)
+
+  //     const botMessages = [
+  //       { text: data.answer, sender: "bot" },
+  //       match && {
+  //         text: <p dangerouslySetInnerHTML={{ __html: data.answer }}></p>,
+  //         sender: "bot",
+  //       },
+  //     ].filter(Boolean)
+
+  //     setQuestions((prevQuestions) => [...prevQuestions, ...botMessages])
+  //   } else {
+  //     console.error("Invalid response format:", data)
+  //   }
+  // }
+
   const handleResponse = (data) => {
     if (data && data.answer) {
-      const dynamicUrlRegex = /<a href="([^"]+)" target="_blank">([^<]+)<\/a>/
-      const match = data.answer.match(dynamicUrlRegex)
+      const dynamicUrlRegex = /<a href="([^"]+)" target="_blank">([^<]+)<\/a>/g
+      const matches = Array.from(data.answer.matchAll(dynamicUrlRegex))
 
-      const botMessages = [
-        { text: data.answer, sender: "bot" },
-        match && {
-          text: (
-            <p>
-              <a href={match[1]} target="_blank" rel="noopener noreferrer">
-                {match[2]}
-              </a>
-            </p>
-          ),
+      const elements = []
+      let lastIndex = 0
+
+      for (const match of matches) {
+        const [fullMatch, href, linkText] = match
+        const plainText = data.answer.substring(lastIndex, match.index)
+
+        // Add plain text
+        elements.push(<span key={`plain-${lastIndex}`}>{plainText}</span>)
+
+        // Add clickable link
+        elements.push(
+          <a
+            key={`link-${lastIndex}`}
+            href={href}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {linkText}
+          </a>
+        )
+
+        lastIndex = match.index + fullMatch.length
+      }
+
+      // Add the remaining plain text
+      const remainingText = data.answer.substring(lastIndex)
+      elements.push(<span key={`plain-${lastIndex}`}>{remainingText}</span>)
+
+      setQuestions((prevQuestions) => [
+        ...prevQuestions,
+        {
+          text: <p>{elements}</p>,
           sender: "bot",
         },
-      ].filter(Boolean)
-
-      setQuestions((prevQuestions) => [...prevQuestions, ...botMessages])
+      ])
     } else {
-      console.error("Invalid response format:", data)
+      // Render plain text if there is no link
+      setQuestions((prevQuestions) => [
+        ...prevQuestions,
+        { text: data.answer, sender: "bot" },
+      ])
     }
   }
 
