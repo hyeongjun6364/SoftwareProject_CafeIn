bc50d68d3e4f96d876e9c19d1ff4eba2a4a7844f
kscold <chan6502@gmail.com>
2023-11-20 03:21:30 +0900

    Update mypage.jsx

--- cafein/src/page/mypage/mypage.jsx
+++ cafein/src/page/mypage/mypage.jsx
@@ -123,7 +123,8 @@ const MyPage = () => {
   console.log("usequery:", communityPosts)
   useEffect(() => {
     if (!isLoading && !isFetching) {
-      const name = storedUsername.slice(1, -1)
+      // const name = storedUsername.slice(1, -1)
+      const name = storedUsername ? storedUsername.slice(1, -1) : ""
 
       if (Array.isArray(communityPosts)) {
         const myPosts = communityPosts.filter(
